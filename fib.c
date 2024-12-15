f14035e3712850a796e63f55c2c360247595be0f
ARTURO-RM-101 <aroman694@csu.fullerton.edu>
2024-12-14 17:54:03 -0800

    Saved file with fixed formatting

--- fib.c
+++ fib.c
@@ -7,10 +7,8 @@
 unsigned long long int Fibonacci_Recursive_Memo(int fib_num);
 
 // This list vairable would be our cache storage to be later used by memoization and wrapper functions
-unsigned long long int *cache_storage;
+unsigned long long int *Cache_Storage;
 
-// Iterates the function by N placed into int X
-// Variables are set to keep track of current and previous numbers to allow Fibonacci
 unsigned long long int Fibonacci_Iterative(int fib_num) {
    unsigned long long int prev_num = 0;
    unsigned long long int save_num = 0;
@@ -33,7 +31,6 @@ unsigned long long int Fibonacci_Iterative(int fib_num) {
 }
 
 // The entirety of Fibonacci_Recursive calls Memo recursively
-// With N approaching to it's total amount after several addings of previous numbers
 unsigned long long int Fibonacci_Recursive (int fib_num) {
    if (fib_num == 1) {
       return 0;
@@ -44,88 +41,83 @@ unsigned long long int Fibonacci_Recursive (int fib_num) {
    }
 }
 
-// Checks whether the Nth fibonacci was already calculated with not equalling -1
-// If it isn't then it'll be calculated and stored on the cache storage
+// If it isn't calculated then it'll be calculated and stored on the cache storage
 unsigned long long int Fibonacci_Recursive_Memo(int fib_num) {
-   if (cache_storage[fib_num] != -1) {
-      return cache_storage[fib_num];
+
+   if (Cache_Storage[fib_num] != -1) {
+      return Cache_Storage[fib_num];
    }
    if (fib_num == 1) {
-      cache_storage[1] = 0;
+      Cache_Storage[1] = 0;
       return 0;
    }
    if (fib_num == 2) {
-      cache_storage[2] = 1;
+      Cache_Storage[2] = 1;
       return 1;
    }
    
-   cache_storage[fib_num] = Fibonacci_Recursive(fib_num - 1) + Fibonacci_Recursive(fib_num - 2);
+   Cache_Storage[fib_num] = Fibonacci_Recursive(fib_num - 1) + Fibonacci_Recursive(fib_num - 2);
 
-   // Later returns the state of the cache over to the wrapper function
-   return cache_storage[fib_num];
+   return Cache_Storage[fib_num];
+
 }
 
 unsigned long long int Fibonacci_Recursive_Wrapper(int fib_num) {
-      // This is a way to set the whole size of the cache
-      cache_storage = malloc((fib_num + 1) * sizeof(unsigned long long int));
 
-      // This loop is intended to set every position from the given Nth
+      Cache_Storage = malloc((fib_num + 1) * sizeof(unsigned long long int));
+
       // -1 represents Nth positions that haven't been calculated yet
       for (int ix = 0; ix <= fib_num; ix++) {
-         cache_storage[ix] = -1;
+         Cache_Storage[ix] = -1;
       }
 
-      // TotalFibonacci is received by using Memoization with the Nth amount
       unsigned long long int total_fibonacci = Fibonacci_Recursive_Memo(fib_num);
 
-      // Once everything is done with the cache, it then frees up the memory
-      free(cache_storage);
+      free(Cache_Storage);
 
-      // Returns the total fibonacci of the Nth amount the user inputted
       return total_fibonacci;
+      }
+
+unsigned long long int Fibonacci_Iterative_Memo(int fib_num) {
+
+   if (Cache_Storage[fib_num] != -1) {
+      return Cache_Storage[fib_num];
    }
 
-   unsigned long long int Fibonacci_Iterative_Memo(int fib_num) {
-   if (cache_storage[fib_num] != -1) {
-      return cache_storage[fib_num];
-   }
    if (fib_num == 1) {
-      cache_storage[1] = 0;
+      Cache_Storage[1] = 0;
       return 0;
    }
+
    if (fib_num == 2) {
-      cache_storage[2] = 1;
+      Cache_Storage[2] = 1;
       return 1;
    }
    
-   cache_storage[fib_num] = Fibonacci_Iterative(fib_num);
+   Cache_Storage[fib_num] = Fibonacci_Iterative(fib_num);
 
-   // Later returns the state of the cache over to the wrapper function
-   return cache_storage[fib_num];
+   return Cache_Storage[fib_num];
+
 }
 
-   unsigned long long int Fibonacci_Iterative_Wrapper(int fib_num) {
-      // This is a way to set the whole size of the cache
-      cache_storage = malloc((fib_num + 1) * sizeof(unsigned long long int));
+unsigned long long int Fibonacci_Iterative_Wrapper(int fib_num) {
 
-      // This loop is intended to set every position from the given Nth
-      // -1 represents Nth positions that haven't been calculated yet
-      for (int ix = 0; ix <= fib_num; ix++) {
-         cache_storage[ix] = -1;
-      }
+   // This is a way to set the whole size of the cache
+   Cache_Storage = malloc((fib_num + 1) * sizeof(unsigned long long int));
 
-      // TotalFibonacci is received by using Memoization with the Nth amount
-      unsigned long long int total_fibonacci = Fibonacci_Recursive_Memo(fib_num);
-
-      // Once everything is done with the cache, it then frees up the memory
-      free(cache_storage);
-
-      // Returns the total fibonacci of the Nth amount the user inputted
-      return total_fibonacci;
+   for (int ix = 0; ix <= fib_num; ix++) {
+      Cache_Storage[ix] = -1;
    }
 
-// Main paramaters are utilized to grab command 
-// line string to be used through this part
+   // TotalFibonacci is received by using Memoization with the Nth amount
+   unsigned long long int total_fibonacci = Fibonacci_Recursive_Memo(fib_num);
+
+   // Once everything is done with the cache, it then frees up the memory
+   free(Cache_Storage);
+
+   return total_fibonacci;
+}
+
 int main(int argc, char* argv[]) {
    
    // Text processing code is commented for testing
@@ -148,14 +140,14 @@ int main(int argc, char* argv[]) {
          return 1;
    }*/
 
-   // Both the user command line and the text file numbers are added together
    unsigned long long int fib_num = atoi(argv[1]);
-   // N is subtracted by 1 as a required thing in Fibonacci sequencing
 
    if(strcmp(argv[2], "i") == 0) {
         printf("%llu\n", Fibonacci_Iterative_Wrapper(fib_num)); 
    } else if(strcmp(argv[2], "r") == 0) {
         printf("%llu\n", Fibonacci_Recursive_Wrapper(fib_num));
    }
+
    return 0;
+
 }
